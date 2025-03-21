23818017dbf9173cfc6ebc2026bfadba24738484
cajimenez96 <carlosj_utn@hotmail.com>
2025-03-21 20:40:40 -0300

    Se agrega componente

---
+++ src/components/Button/CustomButton.tsx
@@ -0,0 +1,25 @@
+import { CSSProperties, ReactNode } from 'react';
+import { Button } from 'antd';
+
+interface CustomButtonProps {
+  children: ReactNode;
+  style?: CSSProperties;
+  handleClick: () => void;
+}
+
+const CustomButton = ({ children, style, handleClick }: CustomButtonProps) => {
+  const defaultStyle = {
+    paddingTop: 20,
+    paddingBottom: 20,
+    paddingLeft: 30,
+    paddingRight: 30,
+  };
+
+  return (
+    <Button type='primary' style={{...defaultStyle, ...style}} onClick={handleClick}>
+      {children}
+    </Button>
+  )
+}
+
+export default CustomButton
