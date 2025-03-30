daa57f044bff48b1c6a61a1ebc33bbcfa6816650
cajimenez96 <carlosj_utn@hotmail.com>
2025-03-30 00:47:13 -0300

    Se agrega traducciones

--- src/main.tsx
+++ src/main.tsx
@@ -6,23 +6,26 @@ import App from './App.tsx';
 import { persistor, store } from './redux/store.ts';
 import './index.css';
 import { Toaster } from 'sonner';
+import { i18n, I18nextProvider } from './lib/i18n.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <App />
-        <Toaster
-          duration={2000}
-          toastOptions={{
-            style: {
-              background: 'rgba(22, 72, 99, 0.7)',
-              color: '#fff',
-              fontWeight: 900,
-              padding: '1rem',
-            },
-          }}
-        />
+        <I18nextProvider i18n={i18n}>
+          <App />
+          <Toaster
+            duration={2000}
+            toastOptions={{
+              style: {
+                background: 'rgba(22, 72, 99, 0.7)',
+                color: '#fff',
+                fontWeight: 900,
+                padding: '1rem',
+              },
+            }}
+          />
+        </I18nextProvider>
       </PersistGate>
     </Provider>
   </React.StrictMode>
