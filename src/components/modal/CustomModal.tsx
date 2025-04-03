7df05e47ae279c046eb668c794464cb7dab9f69e
cajimenez96 <carlosj_utn@hotmail.com>
2025-04-03 10:35:19 -0300

    Se crea componente nuevo

---
+++ src/components/modal/CustomModal.tsx
@@ -0,0 +1,45 @@
+import { Flex, Modal } from 'antd';
+import { ReactNode } from 'react';
+
+interface CustomModalProps {
+  title: string;
+  show: boolean;
+  center?: boolean
+  children: ReactNode;
+  footer?: ReactNode;
+  closable?: boolean;
+  handleOk?: ((e: React.MouseEvent<HTMLButtonElement>) => void);
+  handleCancel?: ((e: React.MouseEvent<HTMLButtonElement>) => void);
+  handleClose: () => void;
+}
+
+const CustomModal = ({
+  title,
+  show,
+  center = true,
+  footer,
+  children,
+  closable = false,
+  handleOk,
+  handleCancel,
+  handleClose
+}: CustomModalProps) => {
+  return (
+    <Modal
+    title={title}
+    centered={center}
+    open={show}
+    closable={closable}
+    onClose={handleClose}
+    onOk={handleOk}
+    onCancel={handleCancel}
+    footer={footer ? footer : null}
+  >
+    <Flex align='center' justify='center' style={{ padding: 20, minHeight: 300 }}>
+      {children}
+    </Flex>
+  </Modal>
+  )
+}
+
+export default CustomModal
